d65e28b917c1e165672aab66dde95788c4c7b591
20toulemon <71647994+20toulemon@users.noreply.github.com>
2021-01-10 22:34:59 +0100

    Update ProjNum2.py

--- ProjNum2.py
+++ ProjNum2.py
@@ -15,3 +15,15 @@ def solve_euler_implicit(f, x0, dt, t0, tf, itermax = 100):
         x.append(x[-1] + dt*f(t[-1], x1))
         t.append(t[-1] + dt)
     return t, x
+
+#question 6, ordre de convergence
+def exp(t, x):
+    return x
+
+for i in range(1, 6):
+    dt = 2**(-i)
+    plt.plot(solve_euler_explicit(exp, 1, dt, 0, 5), label = f'dt = 2^{-i}')
+t = solve_euler_explicit(exp, 1, dt, 0, 5)[0]
+plt.plot(t, np.exp(t), label='exp')
+plt.legend()
+plt.show()
