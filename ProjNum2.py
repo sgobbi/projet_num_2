f575d6ec92f9325912f33117eb75ff86dbde7356
20toulemon <71647994+20toulemon@users.noreply.github.com>
2021-01-10 21:29:15 +0100

    Update ProjNum2.py

--- ProjNum2.py
+++ ProjNum2.py
@@ -7,5 +7,11 @@ def solve_euler_explicit(f, x0, dt, t0, tf):
     return t, x
 
 def solve_euler_implicit(f, x0, dt, t0, tf, itermax = 100):
-    
+        t = [t0]
+    x = [x0]
+    iter = 0
+    while t[-1] < tf and i < itermax:
+        x1 = x[-1] + dt*f(t[-1], x[-1])
+        x.append(x[-1] + dt*f(t[-1], x1))
+        t.append(t[-1] + dt)
     return t, x
