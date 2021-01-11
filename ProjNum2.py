30b7fb43e6e867cb1490477173eed88c51d800ce
20toulemon <71647994+20toulemon@users.noreply.github.com>
2021-01-11 01:10:00 +0100

    Update ProjNum2.py

--- ProjNum2.py
+++ ProjNum2.py
@@ -27,3 +27,12 @@ t = solve_euler_explicit(exp, 1, dt, 0, 5)[0]
 plt.plot(t, np.exp(t), label='exp')
 plt.legend()
 plt.show()
+
+Réponse à la question 11
+K est le facteur responsable de la vitesse de convergence de H vers H_{}. Plus k est grand et plus la solution converge rapidement.
+Il ne peut cependant pas être choisi arbitrairement grand, car la correction pourrait alors être trop forte par rapport au pas de temps, ce qui rendrait la méthode d'Euler inefficace :
+$\forall j, H(x_{j+1}) - H_{0} =  H(x_{j}) - H_{0} + dt(-k)(||\nabla H(x_{j})||)^2(H(x_{j+1}) - H_{0})
+H(x_{j+1}) - H_{0} = (H(x_{j+1}) - H_{0})(1-kdt(||\nabla H(x_{j})||)^2)$
+Oron sait que $(||\nabla H(x_{j})||)^2$ est borné d'après les questions précédentes. En considéran ses bornes, on retrouve une suite géométrique de raison $(1-kdt(||\nabla H(x_{j})||)^2)$.
+Or pour que la solution converge bien, il faut $|(1-kdt(||\nabla H(x_{j})||)^2)|<1$. C'est-à-dire lorsque $0<k<\fract{2}{dt(||\nabla H(x_{j})||)^2$
+          
